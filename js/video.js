dbfa4d4478c0b296cff7d135297f52e146f9f901
carlos cezar <carlosjsciaa@gmail.com>
2023-04-13 00:00:27 -0300

    adicionado os arquivos para meu diretório do git para testar.

---
+++ js/video.js
@@ -0,0 +1,25 @@
+
+
+let video = document.getElementsByClassName("video-screen")[0];
+function play(){
+    video.play();
+}
+var temp = 0;
+function tempo(temp){
+    video.play();
+    video.currentTime = temp;
+    
+    
+}
+let video1 = document.getElementsByClassName("video-screen1")[0];
+function play(){
+    video1.play();
+}
+var temp = 0;
+function tempo(temp){
+    video.play();
+    video.currentTime = temp;
+    
+    
+}
+
