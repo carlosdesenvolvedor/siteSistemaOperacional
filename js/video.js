d822d8dab817d45dc71ed8696e043031f4927619
carlos cezar <carlosjsciaa@gmail.com>
2023-04-14 00:29:29 -0300

    Fiz várias coisas legais, como  incluir os videos que estavam faltando nos módulos III IV e V.

--- js/video.js
+++ js/video.js
@@ -11,15 +11,48 @@ function tempo(temp){
     
     
 }
-let video1 = document.getElementsByClassName("video-screen1")[0];
-function play(){
-    video1.play();
+function pause(){
+    video.pause();
 }
-var temp = 0;
-function tempo(temp){
+/* Testar essa função para parar o video*/
+/*Esse codígo manda mensagem de tem po em tempo*/
+/*
+setTimeout(function() {
+    alert("acabou o tempo")
+}, 5000);
+*/
+var f = 0;
+function prog(temp,f){
     video.play();
     video.currentTime = temp;
+            setTimeout(function() {
+            video.currentTime = 6540;
+            var conf = confirm("Assitir essa parte novamente?", true);
+            if(conf == true){
+                
+                prog(temp,f);
+            }
+            else{
+                /*var prox = confirm("voltar para proximo slid",true);*/
+                video.currentTime = 6540;
+                if(prox == true){
+                  /*  alert("indo para proximo slid");
+                    window.scroll(1500,6500)
+                    
+                */
+                }
+                else{
+                  /*  alert("sair");*/
+                }
+            }
     
+        }, f);
     
 }
 
+
+
+(function duracao() {
+    var obj = document.getElementById("video-screen");
+    console.log(obj.currentTime, obj.duration, obj.ended);
+})(2000);
