0063efcb913b6753a3f89300e12f8657b47d04ba
carlos cezar <carlosjsciaa@gmail.com>
2023-04-28 12:09:56 -0300

    primeiro simulado para tópico 1

---
+++ sim_mod_1/js/question.js
@@ -0,0 +1,114 @@
+// uma matriz de objetos:
+const quiz = [
+
+
+
+
+{
+    q:'O que é um Hardware?',
+    options:['parte física do computador. É tudo aquilo que pode ser tocado. Exemplo: teclado, mouse, monitor, gabinete, processador, memória entre outros.',
+             'parte virtual, tudo aquilo que não posso tocar como sonar, word, aplicativos etc.',
+             'Sistemas operacionais',
+            'componentes fisicos',
+            'O computador',
+            ],
+    answer: 0
+
+},
+
+{
+    q:'O que é software?',
+    options:['parte palpaver do computador. ',
+             'Parte palpável do computador, onde acessamos nossos arquivos.',
+             'O software é a linguagem de programação.',
+            'parte física do computador. É tudo aquilo que pode ser tocado. Exemplo: teclado, mouse, monitor, gabinete, processador, memória entre outros.',
+            'parte lógica do computador. Ao contrário do hardware, esta parte não pode ser tocada. Também chamado de programa ou aplicativo. Exemplo: sistemas operacionais, processadores de textos, arquivos salvos, planilhas eletrônicas entre outros.',
+            ],
+    answer: 4
+
+},
+{
+    q:'Qual a função de um SO',
+    options:['Ocultar recursos e atrapalhar o funcionamento do computador. ',
+             'Garantir a disponibilidade de recusos, consistência de dados do ponto de vista de quem utiliza esses recursos.',
+             'Criar camadas de abstração na linguagem de programação.',
+            'embarcar o desenvolvimente de hardware',
+            'Fornecer acesso a diversas tecnologias como usb, pci, sata ect. á que exigirá maior poder de processamento e controle do sistema operacional além da utilização de pessoas especializadas.',
+            ],
+    answer: 1
+
+},
+{
+    q:'Uma função do SO é Garantir a disponibilidade de recusos, consistência de dados, uma segunda função seria: ',
+    options:['Criar uma camada de operações que está relacionado ao shell.',
+             'Aumentar as dificuldades da maquina, deixando o sistema mais vulnerável.',
+             'Uma camada de abstração no desenvolvimento e também na utilização da maquina. ',
+            'Criar animações para o usuario e para profissionais da área de TI',
+            'Espera que o desenvlvedor reserve a memória necessaria para seu programa.',
+            ],
+    answer: 2
+
+},
+{
+    q:'Quais são as camadas de um software?',
+    options:['Modo front-end e modo núcleo ',
+             'Modo back-end e modo usuário',
+             'Modo de abstração e modo de hardware',
+            'modo núcleo e modo central',
+            'modo usuário e modo núcleo',
+            ],
+    answer: 2
+
+}, 
+{
+    q:'Em qual camada o sistema operacional opera no software?',
+    options:['Usuário',
+             'Front-end',
+             'Núcleo',
+            'do programa de interface com usuario',
+            'navegador web',
+            ],
+    answer: 2
+
+},
+{
+    q:'O que o modo núcleo do software faz?',
+    options:['Tem mais privilégios, tem maior autonomia para lidar com a camada inferior, que é a camada de hardware.',
+             'Tem menos privilégios, tem maior autonomia para lidar com a camada inferior, que é a camada de software.',
+             'Tem mais privilégios, tem maior autonomia para lidar com a camada inferior, que é a camada de software.',
+            'Tem menos privilégios, tem maior autonomia para lidar com a camada inferior, que é a camada de hardware.',
+            'Nenhuma das alternativas.',
+            ],
+    answer: 0
+
+},
+{
+    q:'O que tem no modo usuário do software?',
+    options:['Navegador web, leitor de e-mail, reprodutor de música',
+             'comandos básicos',
+             'código fonte ',
+            'console.log()',
+            'printf();',
+            ],
+    answer: 0
+
+},
+{
+    q:'Como é conhecida a interface gráfica?',
+    options:['shell',
+             'script',
+             'Java',
+            'GUI',
+            'nenhuma das alternativas',
+            ],
+    answer: 3
+
+}
+
+
+
+
+
+
+
+]
