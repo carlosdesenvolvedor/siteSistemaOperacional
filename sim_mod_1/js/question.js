0b1858eb482546a974e9fded9551ec7ea9f7b1fd
carlos <carlosjs_cia@hotmail.com>
2023-06-03 22:07:41 -0300

    tentativa de mudar simulado

--- sim_mod_1/js/question.js
+++ sim_mod_1/js/question.js
@@ -1,122 +1,143 @@
 // uma matriz de objetos:
 const quiz = [
-
-
-
-
 {
-    q:'O que é um Hardware?',
-    options:['parte física do computador. É tudo aquilo que pode ser tocado. Exemplo: teclado, mouse, monitor, gabinete, processador, memória entre outros.',
-             'parte virtual, tudo aquilo que não posso tocar como sonar, word, aplicativos etc.',
-             'Sistemas operacionais',
-            'componentes fisicos',
-            'O computador',
+    q:'Um processo pode ser iniciado e terminado. O sistema operacional sabe se o processo está em execução ou não, por meio da técnica de troca de estados. Considerando o texto acima e que os processos possuem tipos de estados que podem se encontrar, selecione a alternativa que apresente todos os tipos de estados válidos:',
+    options:['Novo, em heap, em processamento, pronto ',
+             'Em heap, em processamento, pronto, aguardando,',
+             'Em memória, em processamento, pronto, em heap',
+            'Novo, em espera, pronto, em execução',
+            'Novo, pronto, inicial, em memória',
+            ],
+    answer: 3
+
+},
+{
+    q:'Um processo pode ser considerado um programa em execução. Por exemplo: imagine que desejamos ler um livro em nosso computador (os chamados e-books). O livro somente poderá ser lido quando ele estiver em execução e para isto é necessário clicar duas vezes sob ele para a sua abertura. Com isto, um processador de texto será aberto. A partir deste momento temos um processo em execução (no caso o processador de texto com o e-book sendo exibido).'
+ 
+    +'VOLTZ, Wagner Mendes. Sistemas Operacionais. Reimpressão - Maringá - PR. Unicesumar, 2019 (adaptado).'
+    
+    +'Considerando o texto acima e no que diz respeito aos processos de um sistema operacional é correto afirmar que:0',
+    options:['Para a execução de um processo se faz necessário recursos como: tempo de CPU, dispositivos de entrada e saída, alocação de memória e arquivos',
+             'Os processos são estruturas físicas que os operadores devem reproduzir com os computadores,',
+             ' São estruturas abstratas que são executados apenas em ambientes na nuvem',
+            'São estruturas que utilizam o uso do processador e da memória de um computador',
+            ' São estruturas físicas em que se pode expandir a capacidade do hardware para a',
             ],
     answer: 0
 
 },
-
 {
-    q:'O que é software?',
-    options:['parte palpaver do computador. ',
-             'Parte palpável do computador, onde acessamos nossos arquivos.',
-             'O software é a linguagem de programação.',
-            'parte física do computador. É tudo aquilo que pode ser tocado. Exemplo: teclado, mouse, monitor, gabinete, processador, memória entre outros.',
-            'parte lógica do computador. Ao contrário do hardware, esta parte não pode ser tocada. Também chamado de programa ou aplicativo. Exemplo: sistemas operacionais, processadores de textos, arquivos salvos, planilhas eletrônicas entre outros.',
+    q:'Sobre o Sistema Operacional Android é correto afirmar:',
+    options:['É um sistema operacional servidor que é baseado em Linux e foi desenvolvido pelo Google ',
+             'É um sistema operacional embarcado que é focado em dispositivos de Internet das Coisas e sua interface gráfica é baseada no Linux',
+             'É um sistema operacional para dispositivos móveis que teve seu desenvolvimento a partir do kernel do Linux',
+            'É um sistema operacional embarcado que é focado em dispositivos móveis e sua interface gráfica é baseada no Linux',
+            'É um sistema operacional para dispositivos móveis que teve seu desenvolvimento a partir do kernel do Windows',
             ],
-    answer: 4
+    answer: 2
 
 },
 {
-    q:'Qual a função de um SO',
-    options:['Ocultar recursos e atrapalhar o funcionamento do computador. ',
-             'Garantir a disponibilidade de recusos, consistência de dados do ponto de vista de quem utiliza esses recursos.',
-             'Criar camadas de abstração na linguagem de programação.',
-            'embarcar o desenvolvimente de hardware',
-            'Fornecer acesso a diversas tecnologias como usb, pci, sata ect. á que exigirá maior poder de processamento e controle do sistema operacional além da utilização de pessoas especializadas.',
+    q:'Você já deve ter percebido que os processos de alguma maneira têm que se comunicar com outros. Cabe ao sistema operacional garantir esta comunicação de forma bem estruturada e sem interrupções.'
+    + 'Considerando o texto acima e pensando sobre a solução de Peterson é correto afirmar',
+    options:[' É uma solução para facilitar o processo de instalação de múltiplos sistemas operacionais em um computador',
+             'É responsável pela melhor utilização da memória em sistemas operacionais de 64 bits',
+             'Está diretamente ligado com a virtualização de ambientes e na criação de máquinas virtuais',
+            'Consiste em um algoritmo, inicialmente para dois processos, que controla a lista de processos interessados em acessar a região crítica',
+            'Nenhuma das anteriores',
+            ],
+    answer: 3
+
+},
+{
+    q:'A partir da ideia de memória virtual, observe as afirmativas a seguir:'
+    +'I – O controle do uso de memória para a troca de processos armazenados temporariamente realiza trocas de processos na memória (swapping).'
+    +'II – Um dos algoritmos para controle do swapping retira conteúdos de páginas de memória principal menos utilizados e os coloca em memória virtual para que um novo conteúdo seja colocado.'
+    +'III – Conteúdos de páginas de memória em uso são prioridade para serem alocados em memória virtual nos algoritmos de controle de swapping.',
+    options:['I, apenas. ',
+             'II, apenas.',
+             'III, apenas.',
+            'I e II, apenas.',
+            'II e III, apenas.',
+            ],
+    answer: 3
+
+},
+{
+    q:'Podemos descrever a função do sistema operacional como: gerenciar o uso dos componentes (hardware) de um computador, garantindo disponibilidade do hardware e armazenamento correto de dados; fornecer uma camada de abstração para utilização e desenvolvimento de outros softwares; e fornecer uma interface de acesso para dispositivos com tecnologias distintas como USB e IDE.'
+    +'Considerando o texto descrito a cima e sabendo que existem vários tipos de Sistemas Operacionais, selecione a alternativa que apresente tipos reais:',
+    options:['SO de servidores, SO de Backend, SO embarcado; ',
+             'SO de servidores, SO de Computadores Pessoais, SO embarcado',
+             'SO de potência, SO de Computadores Impessoais, SO embarcado',
+            'SO de Aplicação, SO de Threads, SO embarcado',
+            'SO de servidores, SO de Computadores Pessoais, SO de Threads',
             ],
     answer: 1
 
 },
 {
-    q:'Uma função do SO é Garantir a disponibilidade de recusos, consistência de dados, uma segunda função seria: ',
-    options:['Criar uma camada de operações que está relacionado ao shell.',
-             'Aumentar as dificuldades da maquina, deixando o sistema mais vulnerável.',
-             'Uma camada de abstração no desenvolvimento e também na utilização da maquina. ',
-            'Criar animações para o usuario e para profissionais da área de TI',
-            'Espera que o desenvlvedor reserve a memória necessaria para seu programa.',
+    q:'Todo processo precisa de um espaço em memória durante a sua execução e um pedaço desta memória é compartilhado entre outros processos. Este compartilhamento é chamado de região crítica. A melhor maneira para que não ocorra condição de corrida é realizando a exclusão mútua. '
+    +'Com base no texto acima e no que diz respeito a comunicação entre processos e sobre a exclusão mútua, selecione a alternativa que represente um critério que se deve considerar numa proposta de solução:',
+    options:[ 'Todo processo deve esperar eternamente para entrar em sua região crítica',
+             'Dois processos nunca podem estar simultaneamente em suas regiões críticas',
+            'Tudo pode ser afirmado sobre a velocidade ou sobre o número de CPUs',
+            'Todo processo executando fora de sua região crítica pode bloquear outros processos.',
+            'Nenhuma das anteriores',
             ],
-    answer: 2
+    answer: 1
 
 },
 {
-    q:'Quais são as camadas de um software?',
-    options:['Modo front-end e modo núcleo ',
-             'Modo back-end e modo usuário',
-             'Modo de abstração e modo de hardware',
-            'modo núcleo e modo central',
-            'modo usuário e modo núcleo',
+    q:'Um processo pode ser considerado um programa em execução. Por exemplo: imagine que desejamos ler um livro em nosso computador (os chamados e-books). O livro somente poderá ser lido quando ele estiver em execução e para isto é necessário clicar duas vezes sob ele para a sua abertura. Com isto, um processador de texto será aberto. A partir deste momento temos um processo em execução (no caso o processador de texto com o e-book sendo exibido).'
+    +'Sobre os processos de um Sistema Operacional, selecione a alternativa que apresente as quatro grandes áreas que o compõe?',
+    options:['Seção de dados, seção de informações, heap e pilha ',
+             'Seção de texto, seção de dados, heap e fila',
+             'Seção de texto, seção de dados, máquina virtual e pilha',
+            'Seção de memória, seção de dados, fila e pilha',
+            'Seção de texto, seção de dados, heap e pilha',
+            ],
+    answer: 0
+
+},
+{
+    q:'Os recursos de hardware representam uma série de tecnologia que trabalha em conjunto para que soluções de software possam ser executadas. Um dos recursos de hardware mais conhecidos é a memória de armazenamento. As afirmativas a seguir se referem a características de alguns tipos de memórias.'
+    +'I – Memória ROM ou de massa é usada para armazenamento de quantidades maiores de dados de forma permanente.'
+    +'II – Memória RAM se refere à memória temporária básica de trabalho onde os softwares abertos são inicialmente alocados.'
+    +'III – Memória cache se refere a um tipo de memória de pequena capacidade para dados de uso mais imediato pelo processador.',
+    options:['I, apenas. ',
+             'II, apenas.',
+             'III, apenas.',
+            'I e II, apenas.',
+            'II e III, apenas.',
             ],
     answer: 4
 
-}, 
+},
 {
-    q:'Em qual camada o sistema operacional opera no software?',
-    options:['Usuário',
-             'Front-end',
-             'Núcleo',
-            'do programa de interface com usuario',
-            'navegador web',
+    q:'O sistema operacional possuiu um conjunto grande de algoritmos de escalonamento que podem ser utilizados. Eles são divididos em: algoritmos de escalonamento preemptivo: o processo é executado por um tempo máximo fixado; e algoritmos de escalonamento não preemptivo: o processo é executado até que seja bloqueado. Com base no texto acima e que o escalonamento consiste em utilizar algoritmos para decidir qual processo deve ser utilizado. Um dos algoritmos mais simples de escalonamento é o chamado FCFS. Selecione a alternativa que apresenta uma definição correta sobre esse algoritmo:',
+    options:['São conhecidos como Pilhas e a ideia é que o último processo a entrar é o primeiro a sair ',
+             'O algoritmo FCFS consegue gerenciar os processos de modo que os menores sempre sejam executados primeiro',
+             'O algoritmo FCFS (first come, first served) implementa o conceito de Fila, onde o primeiro a entrar é o primeiro a sair',
+            'São conhecidos como Hash e a ideia é que o último processo a entrar é o primeiro a sair',
+            'O algoritmo FPS consegue gerenciar os processos de modo que os menores sempre sejam executados primeiro',
             ],
     answer: 2
 
-},
-{
-    q:'O que o modo núcleo do software faz?',
-    options:['Tem mais privilégios, tem maior autonomia para lidar com a camada inferior, que é a camada de hardware.',
-             'Tem menos privilégios, tem maior autonomia para lidar com a camada inferior, que é a camada de software.',
-             'Tem mais privilégios, tem maior autonomia para lidar com a camada inferior, que é a camada de software.',
-            'Tem menos privilégios, tem maior autonomia para lidar com a camada inferior, que é a camada de hardware.',
-            'Nenhuma das alternativas.',
-            ],
-    answer: 0
-
-},
-{
-    q:'O que tem no modo usuário do software?',
-    options:['Navegador web, leitor de e-mail, reprodutor de música',
-             'comandos básicos',
-             'código fonte ',
-            'console.log()',
-            'printf();',
-            ],
-    answer: 0
-
-},
-{
-    q:'Como é conhecida a interface gráfica?',
-    options:['shell',
-             'script',
-             'Java',
-            'GUI',
-            'nenhuma das alternativas',
-            ],
-    answer: 3
-
-},
-{
-    q:'Fornecer uma interface de acesso para dispositivos com tecnologias distintas como USB e IDE. é uma função do:',
-    options:['linux',
-             'windows',
-             'Computador',
-            'Sistema operacional',
-            'browser',
-            ],
-    answer: 3
-
 }
 
+/*
+{
+    q:'',
+    options:[' ',
+             '',
+             '',
+            '',
+            '',
+            ],
+    answer: 2
 
+}
+*/
 
 
 
